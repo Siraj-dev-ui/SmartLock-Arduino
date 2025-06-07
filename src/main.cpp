5d4d63473d710913cde066d2bcd939d7b442e3cd
Siraj-dev-ui <sirajalig86@gmail.com>
2025-06-07 13:14:33 +0200

    Siraj Dev Initial

--- src/main.cpp
+++ src/main.cpp
@@ -4,23 +4,25 @@
 #define LED_PIN 2    // GPIO connected to LED (e.g., onboard LED)
 
 void setup() {
-  pinMode(REED_PIN, INPUT_PULLUP); // Reed switch between GND and pin
-  pinMode(LED_PIN, OUTPUT);
+  // pinMode(REED_PIN, INPUT_PULLUP); // Reed switch between GND and pin
+  // pinMode(LED_PIN, OUTPUT);
   Serial.begin(115200);
   Serial.println("Reed Switch Door Sensor Started");
 }
 
 void loop() {
-  int reedState = digitalRead(REED_PIN);
+  // int reedState = digitalRead(REED_PIN);
 
-  if (reedState == HIGH) {
-    // Door is OPEN
-    digitalWrite(LED_PIN, HIGH);
-    Serial.println("Door OPEN");
-  } else {
-    // Door is CLOSED
-    digitalWrite(LED_PIN, LOW);
-    Serial.println("Door CLOSED");
-  }
+  // if (reedState == HIGH) {
+  //   // Door is OPEN
+  //   digitalWrite(LED_PIN, HIGH);
+  //   Serial.println("Door OPEN");
+  // } else {
+  //   // Door is CLOSED
+  //   digitalWrite(LED_PIN, LOW);
+  //   Serial.println("Door CLOSED");
+  // }
+
+  Serial.println("MicroController Working...");
   delay(200); // Small delay for stability
 }
