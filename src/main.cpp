0beabb4770123cef4815be760f88a7de65f1e97f
Siraj-dev-ui <sirajalig86@gmail.com>
2025-06-07 13:11:57 +0200

    Reed Switch Configuired

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,23 @@
 #define LED_PIN 2    // GPIO connected to LED (e.g., onboard LED)
 
 void setup() {
-  // pinMode(REED_PIN, INPUT_PULLUP); // Not needed for now
-  // pinMode(LED_PIN, OUTPUT);        // Not needed for now
+  pinMode(REED_PIN, INPUT_PULLUP); // Reed switch between GND and pin
+  pinMode(LED_PIN, OUTPUT);
   Serial.begin(115200);
   Serial.println("Reed Switch Door Sensor Started");
 }
 
 void loop() {
-  Serial.println("esp32 working");
+  int reedState = digitalRead(REED_PIN);
+
+  if (reedState == HIGH) {
+    // Door is OPEN
+    digitalWrite(LED_PIN, HIGH);
+    Serial.println("Door OPEN");
+  } else {
+    // Door is CLOSED
+    digitalWrite(LED_PIN, LOW);
+    Serial.println("Door CLOSED");
+  }
   delay(200); // Small delay for stability
 }
