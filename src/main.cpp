2a673ebe9ea042185962754add63db13065a0289
Siraj-dev-ui <sirajalig86@gmail.com>
2025-06-14 21:15:25 +0200

    get post api call

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include "sercrets.h"
+
 #include <Arduino.h> // Required for PlatformIO (C++)
 #include <BLEDevice.h>
 #include <BLEUtils.h>
@@ -5,13 +7,18 @@
 
 #include <WiFi.h>
 #include <HTTPClient.h>
+// #include <ArduinoJson.h>
 
 #define REED_PIN 4 // GPIO connected to reed switch
 #define LED_PIN 2  // GPIO connected to LED (e.g., onboard LED)
 
-const char *ssid = "haider";
-const char *password = "123456789";
-const char *apiUrl = "https://smartlock-nestapi.onrender.com/doors/test";
+// // mobile
+// const char *ssid = "haider";
+// const char *password = "123456789";
+
+// home
+// const char *apiUrl = "https://smartlock-nestapi.onrender.com/doors/test";
+const char *apiUrl = "http://192.168.0.104:3000/doors/testpost";
 
 void print(String message)
 {
@@ -45,9 +52,9 @@ void AdvertiseBLE()
   print("Ble Advertising Started...");
 }
 
-void WifiCall()
+void ConnectWifi()
 {
-  WiFi.begin(ssid, password);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to WiFi");
 
   while (WiFi.status() != WL_CONNECTED)
@@ -89,6 +96,68 @@ void MakeRequest()
   delay(10000); // Wait 10 seconds before next request
 }
 
+// String sendGetRequest(const String& url) {
+String sendGetRequest()
+{
+  HTTPClient http;
+
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    http.begin(apiUrl);        // Start connection
+    int httpCode = http.GET(); // Send GET request
+
+    if (httpCode > 0)
+    {
+      String response = http.getString();
+      http.end();      // Free resources
+      return response; // Return response
+    }
+    else
+    {
+      Serial.print("GET request failed. Error: ");
+      Serial.println(httpCode);
+      http.end();
+      return "ERROR: GET failed with code " + String(httpCode);
+    }
+  }
+  else
+  {
+    return "ERROR: WiFi not connected";
+  }
+}
+
+void sendPostRequestWithBooleanSimple(const String &url, bool value)
+{
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    HTTPClient http;
+    http.begin(url);
+    http.addHeader("Content-Type", "application/json");
+
+    // Manually create JSON string
+    String requestBody = String("{\"value\":") + (value ? "true" : "false") + "}";
+
+    int httpResponseCode = http.POST(requestBody);
+
+    if (httpResponseCode > 0)
+    {
+      String response = http.getString();
+      Serial.println("POST success:");
+      Serial.println("Code: " + String(httpResponseCode));
+      Serial.println("Response: " + response);
+    }
+    else
+    {
+      Serial.println("POST failed. Error code: " + String(httpResponseCode));
+    }
+
+    http.end();
+  }
+  else
+  {
+    Serial.println("WiFi not connected");
+  }
+}
 void setup()
 {
   // pinMode(REED_PIN, INPUT_PULLUP); // Reed switch between GND and pin
@@ -101,7 +170,20 @@ void setup()
 
   // calling api over wifi
 
-  WifiCall();
+  ConnectWifi();
+  sendPostRequestWithBooleanSimple(apiUrl, false);
+  // MakeRequest();
+
+  // String apiUrl = "http://example.com/api";
+  // // String result = sendGetRequest(apiUrl);
+  // String result = sendGetRequest();
+
+  // Serial.println("API Response:");
+  // Serial.println(result);
+
+  // Get unique ID
+  // String deviceName = "Device_" + GetEspUniqueId();
+  // print(deviceName);
 }
 
 void loop()
@@ -121,5 +203,5 @@ void loop()
   // Serial.println("MicroController Working...");
   // delay(200); // Small delay for stability
 
-  MakeRequest();
+  // MakeRequest();
 }
---
+++ src/sercrets.h
@@ -0,0 +1,4 @@
+#pragma once
+#define WIFI_SSID "TP-Link_6F56"
+#define WIFI_PASSWORD "Mrshah123"
+#define DEVICE_ID "Device_E0891425BF58"
