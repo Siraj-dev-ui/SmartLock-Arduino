6cda38b1110725719f85db815418af09b0672ec2
Siraj-dev-ui <sirajalig86@gmail.com>
2025-06-25 14:04:05 +0200

    Integrated With App

--- src/main.cpp
+++ src/main.cpp
@@ -12,9 +12,11 @@
 // #define REED_PIN 4 // GPIO connected to reed switch
 // #define LED_PIN 2  // GPIO connected to LED (e.g., onboard LED)
 
-#define REED_PIN 27 // Reed switch (LOW when door closed)
-#define PIR_PIN 33  // PIR sensor (HIGH when motion detected)
-#define LED_PIN 2
+#define REED_PIN 27   // Reed switch (LOW when door closed)
+#define PIR_PIN 33    // PIR sensor (HIGH when motion detected)
+#define LED_PIN 23    // Status LED
+#define LED_LOCK 2    // Status LOCKED
+#define LED_UNLOCK 16 // Status UNLOCKED
 
 // Status LED
 
@@ -40,14 +42,14 @@ void print(String message)
   Serial.println(message);
 }
 
-// String GetEspUniqueId()
-// {
-//   uint64_t chipid = ESP.getEfuseMac();  // Returns the MAC address (48 bits)
-//   char uniqueID[13];                    // 12 characters + null terminator
-//   sprintf(uniqueID, "%012llX", chipid); // Convert to hex string
+String GetEspUniqueId()
+{
+  uint64_t chipid = ESP.getEfuseMac();  // Returns the MAC address (48 bits)
+  char uniqueID[13];                    // 12 characters + null terminator
+  sprintf(uniqueID, "%012llX", chipid); // Convert to hex string
 
-//   return String(uniqueID); // Return as String
-// }
+  return String(uniqueID); // Return as String
+}
 
 void AdvertiseBLE()
 {
@@ -57,7 +59,8 @@ void AdvertiseBLE()
   // BLEDevice::init(deviceName.c_str()); // Convert String to const char*
 
   // for testing
-  BLEDevice::init("Device_E0891425BF58");
+  // BLEDevice::init(DEVICE_ID);
+  BLEDevice::init("Device_F073AF6CDDA0");
 
   // BLEServer *pServer = BLEDevice::createServer();
   BLEDevice::createServer();
@@ -152,12 +155,17 @@ void setup()
   pinMode(REED_PIN, INPUT_PULLUP);
   pinMode(PIR_PIN, INPUT);
   pinMode(LED_PIN, OUTPUT);
+  pinMode(LED_LOCK, OUTPUT);
+  pinMode(LED_UNLOCK, OUTPUT);
 
   Serial.begin(115200);
   Serial.println("\n🚪 SMART LAB MONITORING SYSTEM");
   Serial.println("----------------------------------");
   Serial.println("DOOR STATUS  | OCCUPANCY  | ENERGY MODE");
   Serial.println("----------------------------------");
+  // get esp uniquie id
+
+  // Serial.println(GetEspUniqueId());
 
   // calling api over wifi
 
@@ -165,6 +173,7 @@ void setup()
 
   // bluetooth code
   AdvertiseBLE();
+
   // sendPostRequestWithBooleanSimple(apiUrl, false);
   // MakeRequest("http://192.168.0.104:3000/actions/active-action");
 
@@ -182,82 +191,98 @@ void setup()
 
 void loop()
 {
-  // bool isDoorClosed = (digitalRead(REED_PIN) == LOW);
-  // bool isMotionDetected = (digitalRead(PIR_PIN) == HIGH);
-  // unsigned long currentMillis = millis();
+  bool isDoorClosed = (digitalRead(REED_PIN) == LOW);
+  bool isMotionDetected = (digitalRead(PIR_PIN) == HIGH);
+  unsigned long currentMillis = millis();
 
-  // // Update motion detection
-  // if (isMotionDetected)
-  // {
-  //   lastMotionTime = currentMillis;
-  //   if (energySaveActive)
-  //   {
-  //     energySaveActive = false;
-  //     Serial.println("\n✅ Motion detected - Energy mode OFF");
-  //     Serial.println("----------------------------------");
-  //   }
-  // }
+  // Update motion detection
+  if (isMotionDetected)
+  {
+    lastMotionTime = currentMillis;
+    if (energySaveActive)
+    {
+      energySaveActive = false;
+      Serial.println("\n✅ Motion detected - Energy mode OFF");
+      Serial.println("----------------------------------");
+    }
+  }
 
-  // // Check energy save condition
-  // bool shouldEnergySave = (currentMillis - lastMotionTime > MOTION_TIMEOUT);
+  // Check energy save condition
+  bool shouldEnergySave = (currentMillis - lastMotionTime > MOTION_TIMEOUT);
 
-  // // Energy save activation
-  // if (shouldEnergySave && !energySaveActive)
-  // {
-  //   energySaveActive = true;
-  //   Serial.println("\n🚨 ENERGY SAVING MODE ACTIVATED!");
-  //   Serial.println("--> ACTION REQUIRED: Switch OFF lights and AC");
-  //   Serial.println("----------------------------------");
-  // }
+  // Energy save activation
+  if (shouldEnergySave && !energySaveActive)
+  {
+    energySaveActive = true;
+    Serial.println("\n🚨 ENERGY SAVING MODE ACTIVATED!");
+    Serial.println("--> ACTION REQUIRED: Switch OFF lights and AC");
+    Serial.println("----------------------------------");
+  }
 
-  // // Door state changes
-  // static bool lastDoorState = !isDoorClosed;
-  // if (isDoorClosed != lastDoorState)
-  // {
-  //   lastDoorState = isDoorClosed;
-  //   Serial.println(isDoorClosed ? "✅ Door CLOSED" : "🚪 Door OPENED");
+  // Door state changes
+  static bool lastDoorState = !isDoorClosed;
+  if (isDoorClosed != lastDoorState)
+  {
+    lastDoorState = isDoorClosed;
+    Serial.println(isDoorClosed ? "✅ Door CLOSED" : "🚪 Door OPENED");
 
-  //   // Trigger door alert LED
-  //   if (!isDoorClosed)
-  //   {
-  //     doorAlertTime = currentMillis;
-  //   }
-  // }
+    // Trigger door alert LED
+    if (!isDoorClosed)
+    {
+      print("calling api");
+      SmartLockPostman(String(BASE_URL) + "/actions/smartlock-postman", 3);
+      doorAlertTime = currentMillis;
+    }
+    else
+    {
+      SmartLockPostman(String(BASE_URL) + "/actions/smartlock-postman", 4);
+    }
+  }
 
-  // // LED control
-  // if (energySaveActive)
-  // {
-  //   // Blink LED during energy save
-  //   if (currentMillis - lastBlinkTime > BLINK_INTERVAL)
-  //   {
-  //     ledState = !ledState;
-  //     digitalWrite(LED_PIN, ledState);
-  //     lastBlinkTime = currentMillis;
-  //   }
-  // }
-  // else if (!isDoorClosed && (currentMillis - doorAlertTime < DOOR_ALERT_DURATION))
-  // {
-  //   // Solid LED for door alerts
-  //   digitalWrite(LED_PIN, HIGH);
-  // }
-  // else
-  // {
-  //   digitalWrite(LED_PIN, LOW);
-  // }
+  // LED control
+  if (energySaveActive)
+  {
+    // Blink LED during energy save
+    if (currentMillis - lastBlinkTime > BLINK_INTERVAL)
+    {
+      ledState = !ledState;
+      digitalWrite(LED_PIN, ledState);
+      lastBlinkTime = currentMillis;
+    }
+  }
+  else if (!isDoorClosed && (currentMillis - doorAlertTime < DOOR_ALERT_DURATION))
+  {
+    // Solid LED for door alerts
+    digitalWrite(LED_PIN, HIGH);
+  }
+  else
+  {
+    digitalWrite(LED_PIN, LOW);
+  }
 
-  // // Periodic status update
-  // static unsigned long lastStatusUpdate = 0;
-  // if (currentMillis - lastStatusUpdate > 5000)
-  // {
-  //   lastStatusUpdate = currentMillis;
-  //   Serial.print(isDoorClosed ? "🚪 CLOSED    | " : "🚪 OPEN      | ");
-  //   Serial.print(isMotionDetected ? "👥 OCCUPIED | " : "👤 EMPTY    | ");
-  //   Serial.println(energySaveActive ? "🔴 ENERGY SAVE" : "🟢 NORMAL");
-  // }
+  // Periodic status update
+  static unsigned long lastStatusUpdate = 0;
+  if (currentMillis - lastStatusUpdate > 5000)
+  {
+    lastStatusUpdate = currentMillis;
+    // Serial.print(isDoorClosed ? "🚪 CLOSED    | " : "🚪 OPEN      | ");
+    Serial.print(isMotionDetected ? "👥 OCCUPIED | " : "👤 EMPTY    | ");
+    Serial.println(energySaveActive ? "🔴 ENERGY SAVE" : "🟢 NORMAL");
+
+    print("empty check api called.");
+    if (isMotionDetected)
+    {
+
+      SmartLockPostman(String(BASE_URL) + "/actions/smartlock-postman", 5);
+    }
+    else
+    {
+      SmartLockPostman(String(BASE_URL) + "/actions/smartlock-postman", 6);
+    }
+  }
 
   // Check Active Action
 
-  // MakeRequest(apiUrl);
   MakeRequest(String(BASE_URL) + "/actions/active-action");
 
   Serial.println(activeAction);
@@ -266,16 +291,24 @@ void loop()
   {
   case 1:
     Serial.println("Lock the door.");
+    digitalWrite(LED_UNLOCK, LOW);
     SmartLockPostman(String(BASE_URL) + "/actions/smartlock-postman", 1);
+    print("manage led for door locked");
+    digitalWrite(LED_LOCK, HIGH);
+
     break;
   case 2:
     Serial.println("Unlock the door..");
+
+    digitalWrite(LED_LOCK, LOW);
     SmartLockPostman(String(BASE_URL) + "/actions/smartlock-postman", 2);
+    digitalWrite(LED_UNLOCK, HIGH);
+    print("manage led for door unlocked");
     break;
 
     // default:
     //   break;
   }
-  delay(1000);
+  delay(2000);
   // delay(10000);
 }
