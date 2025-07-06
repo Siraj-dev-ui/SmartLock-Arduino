a5b27e7c1383aff1a5040c10aa4cc213f0da12ab
Siraj-dev-ui <sirajalig86@gmail.com>
2025-07-06 13:19:14 +0200

    secrets file added for Evaluation

---
+++ src/secrets.h
@@ -0,0 +1,8 @@
+#pragma once
+// #define WIFI_SSID "TP-Link_6F56"
+// #define WIFI_PASSWORD "Mrshah123"
+#define WIFI_SSID "haider"
+#define WIFI_PASSWORD "123456789"
+#define DEVICE_ID "Device_E0891425BF58"
+
+#define BASE_URL "https://smartlock-nestapi.onrender.com"
